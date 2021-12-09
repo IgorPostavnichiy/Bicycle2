e3ff4c92c1471bd2005dc49ca9966e9941758c07
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2021-12-09 18:31:26 +0200

    Added project files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.menu-button').on( e: 'click', t: function () {
+		$('.navigation-wrapper').addClass( t: 'active');
+});
+
+	$('.close-menu').on( e: 'click', t: function () {
+		$('.navigation-wrapper').removeClass( t: 'active');
+		})
+});
+
